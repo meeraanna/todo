e9a93054609bf7aea5ec5cd30b6867dd244eba3a
meeraanna <meeraannajoseph2000@gmail.com>
2023-02-21 13:25:02 +0530

    add

---
+++ todoproject/todoapp/views.py
@@ -0,0 +1,67 @@
+from django.urls import reverse_lazy
+
+from .models import Task
+from django.shortcuts import render, redirect
+from .forms import TaskForm
+
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView,DeleteView
+
+# class based genic views creation
+class TaskListView(ListView):
+    model=Task
+    template_name = 'home.html'
+    context_object_name ='TASK' #
+
+class TaskDetailView(DetailView):
+    model=Task
+    template_name='details.html'
+    context_object_name = 'task'
+
+class TaskUpdateView(UpdateView):
+    model=Task
+    template_name = 'update.html'
+    context_object_name ='task'
+    fields=('name','priority','date')
+
+    def get_success_url(self):
+        return reverse_lazy('cbvdetail',Kwargs={'pk':self.object.id})
+
+class TaskDeleteView(DeleteView):
+    model=Task
+    template_name ='delete.html'
+    success_url = reverse_lazy('cbvlist')
+
+# function based views
+
+def demo(request):
+    task1 = Task.objects.all()
+    if request.method=="POST":
+        name=request.POST.get('task')
+        priority= request.POST.get('priority')
+        date=request.POST.get('date')
+        task=Task(name=name,priority=priority,date=date)
+        task.save()
+    return render(request,'home.html',{'TASK':task1}) #
+
+# def details(request):
+#         task1=Task.objects.all()
+#         return render(request,'details.html',{'TASK':task1})
+
+
+def delete(request,taskid):
+    task=Task.objects.get(id=taskid)
+    if request.method =='POST':
+        task.delete()
+        return redirect('/')
+    return render(request,'delete.html')
+
+
+def update(request,id):
+    task=Task.objects.get(id=id)
+    f=TaskForm(request.POST or None,instance=task)
+    if f.is_valid():
+        f.save()
+        return redirect('/')
+    return render(request,'edit.html',{'task':task,'form':f})
